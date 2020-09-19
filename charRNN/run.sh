71565ca9fab02da54bfcdc666173d77b30e988d1
Sirat <sirat.b99@gmail.com>
2020-09-20 00:51:45 +0530

    added model, dataset and more

---
+++ charRNN/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Instructions - https://github.com/ml5js/training-charRNN
+# These are the hyperparameters you can change to fit your data
+python train.py --data_path=./input.txt \
+--rnn_size 64 \
+--num_layers 2 \
+--seq_length 64 \
+--batch_size 32 \
+--output_keep_prob 0.75 \
+--num_epochs 16 \
+--save_checkpoints ./checkpoints \
+--save_model ./models
