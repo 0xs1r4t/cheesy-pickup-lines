01365f66eb9543b9c13155d227debb5d2dacac44
Sirat <sirat.b99@gmail.com>
2020-09-19 15:53:57 +0530

    changed 💘floating hearts💘

--- public/add-ons/hearts.js
+++ public/add-ons/hearts.js
@@ -5,8 +5,7 @@ function createHeart() {
     heart.style.left = Math.random() * 100 + "vw";
     heart.style.animationDuration = Math.random() * 2 + 3 + "s";
 
-    heart.innerText = "💜";
-
+    heart.innerHTML="<img src=\"https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/237/heart-with-arrow_1f498.png\" width=\"20px\" height=\"20px\">";
     document.body.appendChild(heart);
 
     setTimeout(() => {
